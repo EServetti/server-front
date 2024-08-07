450ba0af25f309f2f38d0ae54706d73c94b92bdf
unknown <eservetti2018@gmail.com>
2024-08-07 16:05:42 -0300

    main

--- src/components/main components/create/productCreate.jsx
+++ src/components/main components/create/productCreate.jsx
@@ -5,7 +5,7 @@ function ProductCreate({_id, title, photo, price, description, stock}) {
       <div className="product">
         <h3>{title}</h3>
         <img src={photo} alt={`Immage of product ${title}`}/>
-        <h3>${price}</h3>
+        <h3>$USD {price}</h3>
         <h3>stock: {stock}</h3>
         <h3 className="_id">{_id}</h3>
         <p className="description">{description}</p>
--- src/components/main components/index/product.jsx
+++ src/components/main components/index/product.jsx
@@ -6,7 +6,7 @@ function Product({_id, title, photo, price}) {
       <div className="product">
         <h3>{title}</h3>
         <img src={photo} alt={`Immage of product ${title}`}/>
-        <h3>${price}</h3>
+        <h3>$USD {price}</h3>
         <h3 className="_id">{_id}</h3>
         <ProductButton _id={_id}/>
       </div>
