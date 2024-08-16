3341c9138ab97455df5be6d51d31bacf19f83b5b
unknown <eservetti2018@gmail.com>
2024-08-16 12:13:16 -0300

    main

--- port.js
+++ port.js
@@ -1,3 +1,3 @@
-export const path = "https://server-production-f97c.up.railway.app"
-// export const path = "http://localhost:9000"
+// export const path = "https://server-production-f97c.up.railway.app"
+export const path = "http://localhost:9000"
 
